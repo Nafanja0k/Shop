7b4510e8be6623be5109b308ca80cf2ccdfba402
Nafanja0k <sanja.nafanja@gmail.com>
2015-10-23 13:20:14 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main
 		/*Scanner input = new Scanner(System.in);
 		System.out.print("Enter a number: ");
 		double number1 = input.nextDouble();*/
-		Cart cart = new ArrayList<Product>();
+		Cart cart = new Cart();
 		cart.add(new Product("gazeta",3.5));
 		cart.add(new Fruit("apple", 6.0));
 		
